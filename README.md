a05612008d1e7f581cc95b55ec01e360cd421e27
Yoshiya Hinosawa <stibium121@gmail.com>
2024-04-18 12:50:31 +0900

    feat: init

---
+++ README.md
@@ -0,0 +1,19 @@
+# @kt3k/pack v0.1.0
+
+> Bundle Deno scripts into a single script
+
+## Usage
+
+```
+deno run -A jsr:@kt3k/pack [-h|-v] [-o <filename>] <input-file>
+```
+
+### Options
+
+- `-h` Show help message and exit.
+- `-v` Show version number and exit.
+- `-o <filename>` Specify the output file name. If not specified, the bundle is output to stdout.
+
+## License
+
+MIT
---
+++ main.ts
@@ -0,0 +1,57 @@
+import * as esbuild from "esbuild";
+import { parseArgs } from "@std/cli/parse-args";
+import { denoPlugins } from "@luca/esbuild-deno-loader";
+import * as colors from "@std/fmt/colors";
+
+async function main() {
+  const options = parseArgs(Deno.args, {
+    string: ["o"],
+    boolean: ["v", "h"],
+  });
+
+  if (options.v) {
+    console.log("@kt3k/pack@0.1.0");
+    Deno.exit();
+  }
+
+  if (options.h) {
+    console.log(helpMessage());
+    Deno.exit();
+  }
+
+  if (options._.length === 0) {
+    console.error("No input file specified.");
+    console.log();
+    Deno.exit(1);
+  }
+
+  const inputFiles = options._.map((e) => colors.cyan(String(e))).join(", ");
+  const outputFile = colors.magenta(options.o ?? "STDOUT");
+  console.error(
+    `Bundling the file(s).\ninput: ${inputFiles}\noutput: ${outputFile}`,
+  );
+
+  const _result = await esbuild.build({
+    plugins: [...denoPlugins()],
+    entryPoints: options._.map(String),
+    outfile: options.o,
+    bundle: true,
+    format: "esm",
+  });
+}
+
+function usage() {
+  return "Usage: deno run -A jsr:@kt3k/pack [-h|-v] [-o <filename>] <input-file>";
+}
+
+function helpMessage() {
+  return `${usage()}
+
+Options:
+  -h             Show help message and exit.
+  -v             Show version number and exit.
+  -o <filename>  Specify the output file name.
+                 If not specified, the bundle is output to stdout.`;
+}
+
+await main();
