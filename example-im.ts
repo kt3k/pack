a2b6d3b5684d9b4850de05ffe4ffb700dddfa351
Yoshiya Hinosawa <stibium121@gmail.com>
2024-06-24 13:34:03 +0900

    chore: use no semi

--- example-im.ts
+++ example-im.ts
@@ -1,3 +1,3 @@
-import { encodeHex } from "@std/encoding";
+import { encodeHex } from "@std/encoding"
 
-console.log(encodeHex(Uint8Array.from([0, 1, 2, 3])));
+console.log(encodeHex(Uint8Array.from([0, 1, 2, 3])))
--- main.ts
+++ main.ts
@@ -1,42 +1,42 @@
-import * as esbuild from "esbuild";
-import { parseArgs } from "@std/cli/parse-args";
-import { denoPlugins } from "@luca/esbuild-deno-loader";
-import * as colors from "@std/fmt/colors";
-import { join } from "@std/path/join";
+import * as esbuild from "esbuild"
+import { parseArgs } from "@std/cli/parse-args"
+import { denoPlugins } from "@luca/esbuild-deno-loader"
+import * as colors from "@std/fmt/colors"
+import { join } from "@std/path/join"
 
 async function main() {
   const options = parseArgs(Deno.args, {
     string: ["o"],
     boolean: ["v", "h"],
-  });
+  })
 
   if (options.v) {
-    console.log("@kt3k/pack@0.1.9");
-    Deno.exit();
+    console.log("@kt3k/pack@0.1.9")
+    Deno.exit()
   }
 
   if (options.h) {
-    console.log(helpMessage());
-    Deno.exit();
+    console.log(helpMessage())
+    Deno.exit()
   }
 
   if (options._.length === 0) {
-    console.error("No input file specified.");
-    console.log();
-    console.log(usage());
-    console.log();
-    Deno.exit(1);
+    console.error("No input file specified.")
+    console.log()
+    console.log(usage())
+    console.log()
+    Deno.exit(1)
   }
 
-  const inputFiles = options._.map((e) => colors.cyan(String(e))).join(", ");
-  const outputFile = colors.magenta(options.o ?? "STDOUT");
+  const inputFiles = options._.map((e) => colors.cyan(String(e))).join(", ")
+  const outputFile = colors.magenta(options.o ?? "STDOUT")
   console.error(
     `Bundling the file(s).\ninput: ${inputFiles}\noutput: ${outputFile}`,
-  );
+  )
 
   const denoPluginOpts = {
     configPath: await getDenoJsonPath(),
-  };
+  }
 
   await esbuild.build({
     plugins: [...denoPlugins(denoPluginOpts)],
@@ -44,11 +44,11 @@ async function main() {
     outfile: options.o,
     bundle: true,
     format: "esm",
-  });
+  })
 }
 
 function usage() {
-  return "Usage: deno run -A jsr:@kt3k/pack [-h|-v] [-o <filename>] <input-file>";
+  return "Usage: deno run -A jsr:@kt3k/pack [-h|-v] [-o <filename>] <input-file>"
 }
 
 function helpMessage() {
@@ -58,23 +58,23 @@ Options:
   -h             Show help message and exit.
   -v             Show version number and exit.
   -o <filename>  Specify the output file name.
-                 If not specified, the bundle is output to stdout.`;
+                 If not specified, the bundle is output to stdout.`
 }
 
 async function getDenoJsonPath() {
   try {
-    await Deno.stat("deno.json");
-    return join(Deno.cwd(), "deno.json");
+    await Deno.stat("deno.json")
+    return join(Deno.cwd(), "deno.json")
   } catch {
     // pass
   }
 
   try {
-    await Deno.stat("deno.jsonc");
-    return join(Deno.cwd(), "deno.jsonc");
+    await Deno.stat("deno.jsonc")
+    return join(Deno.cwd(), "deno.jsonc")
   } catch {
     // pass
   }
 }
 
-await main();
+await main()
